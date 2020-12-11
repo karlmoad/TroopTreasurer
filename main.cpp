cc14a9d33ec47a60b3f75a4041062fdc859307b3
Karl <karl.moad@gmail.com>
2020-12-11 14:37:07 -0500

    v1 dev

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <QApplication>
-#include <QMainWindow>
+#include "ui/mainwindow.h"
 
 /* Application entry point */
 int main(int argc, char*argv[]) {
     QApplication a(argc, argv);
-    QMainWindow w;
+    MainWindow w;
     w.show();
     a.setActiveWindow(&w);
     return a.exec();
